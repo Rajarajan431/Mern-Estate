3dd2a61a4b50797630929db3b551398c07b2a4e7
Rajarajan <chesterking836@gmail.com>
2023-11-07 12:42:32 +0530

    create middleware to hande errors

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,7 +1,7 @@
 import User from '../models/userModel.js'
 import bycryptjs from 'bcryptjs'
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, password, email } = req.body;
     const hashedPassword = bycryptjs.hashSync(password, 10)
     const newUser = new User({ username, password: hashedPassword, email }) 
@@ -9,7 +9,7 @@ export const signup = async (req, res) => {
         await newUser.save()
         res.status(201).json('User created successfully')
     } catch (error) {
-        res.status(500).json(error.message)
+        next(error)
     }
    
 }
