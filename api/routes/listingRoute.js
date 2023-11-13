edc4e961a33928717737ef17666bc3838f98a08c
Rajarajan <chesterking836@gmail.com>
2023-11-13 18:39:32 +0530

    create search api route

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -1,6 +1,7 @@
 import express  from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { createListing, deleteListing, getListing, updateListing } from "../controllers/listingController.js";
+import { createListing, deleteListing, getListing, 
+    getListings, updateListing } from "../controllers/listingController.js";
 
 const router = express.Router();
 
@@ -8,5 +9,6 @@ router.post('/create', verifyToken, createListing)
 router.delete('/delete/:id', verifyToken, deleteListing)
 router.post('/update/:id', verifyToken, updateListing)
 router.get('/get/:id', getListing)
+router.get('/get', getListings)
 
 export default router;
