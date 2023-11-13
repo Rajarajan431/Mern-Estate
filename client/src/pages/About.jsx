c330fa2e907ff505ef010a7f4404ab13acc32ed0
Rajarajan <chesterking836@gmail.com>
2023-11-13 20:27:53 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        About MyEstate
+      </h1>
+
+      <p>
+        Welcome to MyEstate, where dreams find their home! At
+         MyEstate, we believe that finding the perfect property is 
+         more than a transaction it's a journey to discover the place that resonates 
+         with your lifestyle and aspirations.
+      </p>
+        <br />
+      <p>
+        Our platform is more than just a real estate website; it's a personalized portal
+         designed to guide you through the exciting adventure of buying, selling, or renting 
+         your dream property. With a commitment to excellence, we strive to redefine the real 
+         estate experience by combining cutting-edge technology with a human touch.
+      </p>
+
+    </div>
   )
 }
